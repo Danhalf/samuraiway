2909c89db9a949a2d22746595d3eaf9037dc030d
Danhalf <danhel@rocketmail.com>
2021-01-22 11:11:05 +0200

    init

---
+++ src/Technologies.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Technologies() {
+  return <div>a ya TECNHOBLYA!11</div>;
+}
+
+export default Technologies;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://www.jootoor.com/wp-content/uploads/2019/03/Logo-Design.jpg"
+        alt="Logo"
+      />
+    </header>
+  );
+}
+
+export default Header;

5fc859b47b183d8601a3e0ef6aaf7ce548ec9ebc
Danhalf <danhel@rocketmail.com>
2021-11-06 18:14:56 +0200

    add axios and user API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,29 +2,31 @@ import style from './Users.module.css'
 
 const Users = (props) => {
 
+    debugger
 
     return <div>
         {
             props.users.map(user =>
-                <div className={ style.user_block } key={ user.id }>
-                    <div>
-                        <img
-                            src={ user.userAvatar }
-                            className={ style.user_avatar }
-                            alt=""
-                        />
-                        { user.fullName }
-                        <div>{ user.location.country } - { user.location.city }</div>
-                    </div>
-                    { user.status }
-                    <br/>
-                    <button
-                        className={ style.user_subscribe_btn }
-                        onClick={ () => props.subscribe(user.id) }
-                    >
-                        { user.subscribed ? 'Unsubscribe' : 'Subscribe' }
-                    </button>
-                </div>
+                    <div>{ user }</div>
+                // <div className={ style.user_block } key={ user.id }>
+                //     <div>
+                //         <img
+                //             src={ user.userAvatar }
+                //             className={ style.user_avatar }
+                //             alt=""
+                //         />
+                //         { user.fullName }
+                //         <div>{ user.location.country } - { user.location.city }</div>
+                //     </div>
+                //     { user.status }
+                //     <br/>
+                //     <button
+                //         className={ style.user_subscribe_btn }
+                //         onClick={ () => props.subscribe(user.id) }
+                //     >
+                //         { user.subscribed ? 'Unsubscribe' : 'Subscribe' }
+                //     </button>
+                // </div>
             )
         }
     </div>
--- src/redux/Reducers/usersReducer.js
+++ src/redux/Reducers/usersReducer.js
@@ -1,51 +1,69 @@
 import { AvatarGenerator } from "random-avatar-generator";
+import axios from "axios";
 
 const TOGGLE_SUBSCRIBE = 'TOGGLE_SUBSCRIBE'
 const SET_USERS = 'SET-USERS'
 
-const idGenerator = () => Math.random().toString(36).substr(2, 20);
-const avatarGenerator = new AvatarGenerator();
+// [
+// {
+//     id: idGenerator(),
+//     fullName: 'Alex',
+//     userAvatar: avatarGenerator.generateRandomAvatar(),
+//     status: 'Im happy now :)',
+//     subscribed: true,
+//     location: {
+//         country: 'UA',
+//         city: 'Zhytomyr'
+//     }
+// }, {
+//     id: idGenerator(),
+//     fullName: 'Merry',
+//     userAvatar: avatarGenerator.generateRandomAvatar(),
+//     status: 'Im very happy !!! :)',
+//     subscribed: false,
+//     location: {
+//         country: 'BU',
+//         city: 'Minsk'
+//     }
+// }, {
+//     id: idGenerator(),
+//     fullName: 'Tony',
+//     userAvatar: avatarGenerator.generateRandomAvatar(),
+//     status: 'To old for this shit 8)',
+//     subscribed: true,
+//     location: {
+//         country: 'USA',
+//         city: 'NY',
+//     }
+// },
+// ]
+
+// const idGenerator = () => Math.random().toString(36).substr(2, 20);
+// const avatarGenerator = new AvatarGenerator();
+
+const URL = `https://randomuser.me/api/`
+
+let usersData = []
+
+const userData = async () => axios.get(URL)
+    .then(response => usersData.push(response.data.results[0]))
+
+for (let i = 0; i < 5; i++) {
+    userData()
+}
+
+console.log(usersData);
 
 let initialState = {
-    users: [
-        {
-            id: idGenerator(),
-            fullName: 'Alex',
-            userAvatar: avatarGenerator.generateRandomAvatar(),
-            status: 'Im happy now :)',
-            subscribed: true,
-            location: {
-                country: 'UA',
-                city: 'Zhytomyr'
-            }
-        }, {
-            id: idGenerator(),
-            fullName: 'Merry',
-            userAvatar: avatarGenerator.generateRandomAvatar(),
-            status: 'Im very happy !!! :)',
-            subscribed: false,
-            location: {
-                country: 'BU',
-                city: 'Minsk'
-            }
-        }, {
-            id: idGenerator(),
-            fullName: 'Tony',
-            userAvatar: avatarGenerator.generateRandomAvatar(),
-            status: 'To old for this shit 8)',
-            subscribed: true,
-            location: {
-                country: 'USA',
-                city: 'NY',
-            }
-        },
-    ],
+
+    users: usersData,
 
     inputMessage: '',
     buttonDisabled() {
         return this.inputMessage.length < 1
     }
 }
+
 
 const usersReducers = (state = initialState, action) => {
 

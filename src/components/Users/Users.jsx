9c7bd286168d7f37c34335cf6aba26f866e2dd0a
Danhalf <danhel@rocketmail.com>
2021-11-10 23:34:29 +0200

    prepare to user pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,47 +1,56 @@
 import style from './Users.module.css'
 import axios from "axios";
 import { AvatarGenerator } from "random-avatar-generator";
+import { Component } from "react";
 
-const Users = (props) => {
+// const avatarGenerator = new AvatarGenerator()
+const pageCount = 100
 
-    if (props.users.length === 0) {
-        const URL = `https://social-network.samuraijs.com/api/1.0/users`
+class Users extends Component {
 
-        axios.get(URL)
-            .then(response => props.setUsers(response.data.items))
+    constructor() {
+        super();
+        this.avatarGenerator = new AvatarGenerator()
     }
 
-    const avatarGenerator = new AvatarGenerator();
+    componentDidMount() {
+        const URL = `https://social-network.samuraijs.com/api/1.0/users?count=${ pageCount }`
 
-    props.users.map(user => user.photos.small = avatarGenerator.generateRandomAvatar())
+        axios.get(URL)
+            .then(response => this.props.setUsers(response.data.items))
+    }
+
+    render() {
 
 
-    return <div>
-        {
-            props.users.map((user) =>
-                <div className={ style.user_block } key={ user.id }>
-                    <div>
-                        <img
-                            src={ user.photos.small }
-                            className={ style.user_avatar }
-                            alt=""
-                        />
-                        { user.name }
-                        {/*<div>{ user.location.country }</div>*/ }
+        return <div>
+            {
+                this.props.users.map((user) =>
+                    <div className={ style.user_block } key={ user.id }>
+                        <div>
+                            <img
+                                src={ user.photos.small ?? this.avatarGenerator.generateRandomAvatar() }
+                                className={ style.user_avatar }
+                                alt=""
+                            />
+                            { user.name }
+                            {/*<div>{ user.location.country }</div>*/ }
+                        </div>
+                        {/*{ user.email }*/ }
+                        <br/>
+                        <button
+                            className={ style.user_subscribe_btn }
+                            onClick={ () => this.props.subscribe(user.id) }
+                        >
+                            { user.subscribed ? 'Unsubscribe' : 'Subscribe' }
+                        </button>
                     </div>
-                    {/*{ user.email }*/ }
-                    <br/>
-                    <button
-                        className={ style.user_subscribe_btn }
-                        onClick={ () => props.subscribe(user.id) }
-                    >
-                        { user.subscribed ? 'Unsubscribe' : 'Subscribe' }
-                    </button>
-                </div>
-            )
-        }
-    </div>
+                )
+            }
 
+        </div>
+
+    }
 }
 
 export default Users
